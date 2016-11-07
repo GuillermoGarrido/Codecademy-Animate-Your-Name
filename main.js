257bb6e470a105cbe949c4c6868f6f0deef58e69
Guillermo Garrido <G6In3D@users.noreply.github.com>
2016-11-06 19:54:26 -0500

    main.js
    This is the Javascript portion of the code used in the "Animate Your Name" project.

---
+++ main.js
@@ -0,0 +1,19 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [250, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Guillermo"; //creates name that will be animated
+var letterColors = [red, orange, green, blue, purple]; //establishes the colors of the animated name
+if(2 < 5) {
+    bubbleShape = "circle";
+} //end if
+
+else {
+    bubbleShape = "square";
+} //end else
+
+
+drawName(myName, letterColors);
+bounceBubbles()
